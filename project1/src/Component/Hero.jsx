a9afa853af94762b12880673312271e2f6fdeddf
Shubham Gupta <guptashubham5991@gmail.com>
2024-01-18 12:09:55 +0530

    first commit

---
+++ project1/src/Component/Hero.jsx
@@ -0,0 +1,25 @@
+export default function Hero() {
+  return (
+    <main>
+      <div className="content">
+    
+          <h2 className="title">YOUR FEET DESERVE THE BEST</h2>
+        
+        <p className="desc">YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+        <div className="shop"><button className="shop_now">Shop Now</button>
+             <button className="category">Category</button>
+        </div>
+        <div className="footer">
+            <p className="avail">Also Available On</p>
+            <div className="e-image">
+                <img src="./images/flipkart.png" alt="flipkart" />
+                <img src="./images/amazon.png" alt="amazon" />
+            </div>
+        </div>
+      </div>
+      <div className="shoe">
+        <img src="./images/shoe_image.png" alt="shoe" />
+      </div>
+    </main>
+  );
+}
---
+++ project1/src/Component/Navbar.jsx
@@ -0,0 +1,19 @@
+
+function Navbar(){
+    return(
+        <nav>
+            <div>
+                <img src="./images/brand_logo.png" alt="logo" />
+            </div>
+            <ul>
+                <li>Menu</li>
+                <li>Location</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            <div><button>Login</button></div>
+        </nav>
+    )
+}
+
+export default Navbar;
